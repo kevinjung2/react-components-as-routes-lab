34eb4a7a0e99bb79c7ada5363583599c57aa47df
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-02 02:18:38 -0400

    commit tree d6a891f6086ed9453798a56b951604f9574729db

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  let directorCards = directors.map(director => {
+    return(
+      <div>
+        <h3>{director.name}</h3>
+        <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorCards}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  let movieCards = movies.map(movie => {
+    return(
+      <div>
+        <h3>{movie.title}</h3>
+        <h3>{movie.time}</h3>
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movieCards}
     </div>
   );
 };
